135c0302bd76e551f08c088a1b1cdf4166eddb51
Nobu-2657 <221205009@ccmailg.meijo-u.ac.jp>
2026-06-16 20:28:08 +0900

    チュートリアルの途中で視点移動を有効化

---
+++ Assets/Scripts/Player/OnPlayerMove.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPlayerMove : MonoBehaviour
+{
+    [SerializeField] private PlayerMove playerMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerMove != null)
+        {
+            playerMove.enabled = true;
+            Debug.Log("有効化しました！");
+        }
+    }
+}
